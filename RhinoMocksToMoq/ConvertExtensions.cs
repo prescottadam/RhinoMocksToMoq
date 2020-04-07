a1f69cbf322d06f91c5b6ad1d6c56c5e9dafd7d6
JF Peyroux <JF.Peyroux@SkyKick.com>
2020-04-07 10:52:53 -0700

    Various bug fixes

--- RhinoMocksToMoq/ConvertExtensions.cs
+++ RhinoMocksToMoq/ConvertExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text.RegularExpressions;
 
 namespace RhinoMocksToMoq
@@ -8,36 +9,37 @@ namespace RhinoMocksToMoq
         public static string RegexReplace(this string input, string pattern, string replacement)
         {
             string result = input;
-            var re = new Regex(pattern, RegexOptions.Singleline);
-            while (re.IsMatch(result))
+            var regex = new Regex(pattern, RegexOptions.Singleline);
+            var matches = regex.Matches(input).Cast<Match>().ToList();
+
+            matches.ForEach(match =>
             {
-                result = re.Replace(result, replacement);
-            }
+                result = regex.Replace(result, replacement);
+            });
 
             return result;
         }
 
         public static string ConvertUsings(this string input)
         {
-            return input
-                .Replace("using Rhino.Mocks;", "using Moq;");
+            return input.Replace("using Rhino.Mocks;", "using Moq;");
         }
 
         public static string ConvertMockingKernel(this string input)
         {
-            var result = 
+            var result =
                 input
                     .Replace("using Ninject.MockingKernel.RhinoMock;", "using Ninject.MockingKernel.Moq;")
                     .Replace("IKernel", "MoqMockingKernel")
                     .Replace("new RhinoMocksMockingKernel", "new MoqMockingKernel");
 
             // look for member-level mocking kernel
-            var memberLevelKernelRegex = new Regex(@"(private|public|internal)\s+MoqMockingKernel\s+(?<varName>\w+)");
+            var memberLevelKernelRegex = new Regex(@"(private|public|internal)\s+MoqMockingKernel\s+(?<varName>[a-zA-Z_0-9]+)");
             if (memberLevelKernelRegex.IsMatch(result))
             {
                 var match = memberLevelKernelRegex.Match(result);
                 var kernelVarName = match.Groups["varName"].Value;
-                result = result.RegexReplace($@"{kernelVarName}\.Get<(I[A-Z]\w+?)>", $"{kernelVarName}.GetMock<$1>");
+                result = result.RegexReplace($@"{kernelVarName}\.Get<([A-Z]([a-zA-Z_0-9.])+?)>", $"{kernelVarName}.GetMock<$1>");
             }
 
             // look for method-level mocking kernel
@@ -46,7 +48,7 @@ namespace RhinoMocksToMoq
             {
                 var match = methodLevelKernelRegex.Match(result);
                 var kernelVarName = match.Groups["varName"].Value;
-                result = result.RegexReplace($@"{kernelVarName}\.Get<(I[A-Z]\w+?)>", $"{kernelVarName}.GetMock<$1>");
+                result = result.RegexReplace($@"{kernelVarName}\.Get<([A-Z]([a-zA-Z_0-9.])+?)>", $"{kernelVarName}.GetMock<$1>");
             }
 
             return result;
@@ -55,6 +57,7 @@ namespace RhinoMocksToMoq
         public static string ConvertMockCreation(this string input)
         {
             return input
+                .Replace("MockRepository.GenerateStub", "new Mock")
                 .Replace("MockRepository.GenerateMock", "new Mock")
                 .RegexReplace(@"MockRepository.GeneratePartialMock<(.*?)>\((.*?)\);", "new Mock<$1>($2) { CallBase = true };");
         }
@@ -65,16 +68,15 @@ namespace RhinoMocksToMoq
 
             // find mocks by assignment e.g. "something = something.GetMock" or "something = new Mock"
             var mockRegex = new Regex(@"(?<varName>\w+)\s+=\s+(((\w+?)\.GetMock)|new Mock)");
-            if (mockRegex.IsMatch(result))
+            var matches = mockRegex.Matches(input).Cast<Match>().ToList();
+
+            foreach (var match in matches)
             {
-                foreach (var match in mockRegex.Matches(result).OfType<Match>())
-                {
-                    var mockVarName = match.Groups["varName"].Value;
-                    result =
-                        result
-                            .RegexReplace($@"([^vd\s]\w+)\s+{mockVarName};", $"Mock<$1> {mockVarName};")
-                            .RegexReplace($@"{mockVarName}((?!\s*\.Setup|\s*\.Verify|\s*\.Object|\s*[=;]))", $"{mockVarName}.Object$1");
-                }
+                var mockVarName = match.Groups["varName"].Value;
+                result =
+                    result
+                        .RegexReplace($@"([^vd\s][A-Z][a-zA-Z_0-9.]+)\s+{mockVarName};", $"Mock<$1> {mockVarName};")
+                        .RegexReplace($@"{mockVarName}((?!\s*\.Setup|\s*\.Verify|\s*\.Object|\s*[=;]))", $"{mockVarName}.Object$1");
             }
 
             return result;
@@ -86,7 +88,9 @@ namespace RhinoMocksToMoq
                 .RegexReplace(@"\.Stub\((.*?)\)(\s*)\.Return\((.*?)\);", ".Setup($1)$2.Returns($3);")
                 .RegexReplace(@"\.Stub\((.*?)\)(\s*)\.WhenCalled\((.*?)\);", ".Setup($1)$2.Returns($3);")
                 .RegexReplace(@"\.Stub\((.*?)\)(\s*)\.Do\((.*?)\);", ".Setup($1)$2.Returns($3);")
-                .RegexReplace(@"\.Stub\((.*?)\)(\s*)\.Throw\((.*?)\);", ".Setup($1)$2.Throws($3);");
+                .RegexReplace(@"\.Stub\((.*?)\)(\s*)\.Throw\((.*?)\);", ".Setup($1)$2.Throws($3);")
+                .RegexReplace(@"\.Setup\((.*?)\)(\s*)\.Throw\((.*?)\);", ".Setup($1)$2.Throws($3);")
+                .RegexReplace(@"\.Expect\((.*?)\)(\s*)\.Throw\((.*?)\);", ".Expect($1)$2.Throws($3);");
         }
 
         public static string ConvertExpects(this string input)
@@ -99,10 +103,16 @@ namespace RhinoMocksToMoq
         public static string ConvertArgumentConstraints(this string input)
         {
             return input
-                .RegexReplace(@"Arg<([^(]+?)>\.Is\.Anything", @"It.IsAny<$1>()")
-                .RegexReplace(@"Arg<([^(]+?)>\.Matches", "It.Is<$1>")
-                .RegexReplace(@"Arg<([^(]+?)>\.Is\.Equal\((.*?)\)", "It.Is<$1>(arg => arg == $2)")
-                .RegexReplace(@"Arg<([^(]+?)>\.Is\.Same\((.*?)\)", "It.Is<$1>(arg => arg == $2)");
+                .RegexReplace(@"ref Arg<([^(,]+?)>\.Ref\(([A-Z0-9a-z_]+?)\)", @"ref $2)")
+                .RegexReplace(@"out Arg\<[^(,]+\>.Out\((.*?)\).Dummy", @"out $1")
+                .RegexReplace(@"Arg.Is\(([^(,]+?)\)", @"It.Is<>(arg => arg == $1)")
+                .RegexReplace(@"Arg<([^(,]+?)>\.Is\.Anything", @"It.IsAny<$1>()")
+                .RegexReplace(@"Arg<([^(,]+?)>\.Is\.Null", @"It.Is<$1>(arg => arg == null)")
+            .RegexReplace(@"Arg<([^(,]+?)>\.Is\.NotNull", @"It.Is<$1>(arg => arg != null)")
+            .RegexReplace(@"Arg<([^(,]+?)>\.Matches", "It.Is<$1>")
+            .RegexReplace(@"Arg<([^(,]+?)>\.Is\.Equal\((.*?)\)", "It.Is<$1>(arg => arg == $2)")
+            .RegexReplace(@"Arg<([^(,]+?)>\.Is\.Same\((.*?)\)", "It.Is<$1>(arg => arg == $2)")
+            .RegexReplace(@"Arg<([^(,]+?)>\.Is\.NotSame\((.*?)\)", "It.Is<$1>(arg => arg != $2)");
         }
 
         public static string ConvertAssertions(this string input)
--- RhinoMocksToMoqConsole/Program.cs
+++ RhinoMocksToMoqConsole/Program.cs
@@ -10,12 +10,15 @@ namespace RhinoMocksToMoqConsole
     {
         static void Main(string[] args)
         {
-            if (!(args.Length == 1  && File.Exists(args[0]) || args.Length == 3 && args[1] == "-out" && File.Exists(args[0]) || args.Length == 2 && args[0] == "-batch" && File.Exists(args[1])))
+            var isBatch = args[0] == "-batch";
+            if (!(args.Length == 1  && File.Exists(args[0]) || args.Length == 3 && args[1] == "-out" && File.Exists(args[0])  || args.Length == 2 && isBatch && File.Exists(args[1])))
             {
                 Console.WriteLine("Usage: RhinoMocksToMoqConsole <filepath> [-out file]");
                 Console.WriteLine("Usage: RhinoMocksToMoqConsole -batch filepath-containing-list-of-files]");
                 Environment.Exit(1);
             }
+
+            var outFilename = !isBatch ? args [2] : null;
             var writeToConsole = args.Length == 1;
             var paths = args[0] == "-batch" ? File.ReadAllLines(args[1]).Where(x => !string.IsNullOrWhiteSpace(x) && x [0] != '#').ToList() : new List<string> { args[0]};
 
@@ -27,7 +30,11 @@ namespace RhinoMocksToMoqConsole
                 if (writeToConsole)
                     Console.WriteLine(newSourceCode);
                 else
-                    File.WriteAllText(filename, newSourceCode);
+                {
+                    Console.WriteLine(filename);
+                    var resultFilename = outFilename ?? filename;
+                    File.WriteAllText(resultFilename, newSourceCode);
+                }
             });
         }
     }
